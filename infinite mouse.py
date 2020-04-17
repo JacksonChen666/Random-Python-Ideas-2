7160ab809a5c618436b74acbf33dc4998a051630
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-04-17 09:02:48 +0200

    ok more

--- infinite mouse.py
+++ infinite mouse.py
@@ -2,6 +2,7 @@
 # key comb: https://github.com/moses-palmer/pynput/issues/20#issuecomment-412714960
 import datetime
 import tkinter as tk
+from platform import system
 
 try:
     from pynput import mouse, keyboard
@@ -32,21 +33,24 @@ current = set()
 
 tnow = datetime.datetime.now()
 tcounter = 0
+screen_width = tk.Tk().winfo_screenwidth()
+screen_height = tk.Tk().winfo_screenheight()
 
 
 def on_move(x, y):
+    # print("{} {}".format(x, y))
     if allowed:  # well that happened
-        if 0.0 <= x <= 0.5:
-            mouse.Controller().move(10000, 0)
+        if -1.0 <= x <= 1:
+            mouse.Controller().move(screen_width - 5, 0)
             print("->", end=" ")
-        elif screen_width - 1 <= x < screen_width:
-            mouse.Controller().move(-10000, 0)
+        elif screen_width - 1 <= x < screen_width + 1:
+            mouse.Controller().move(-screen_width - 5, 0)
             print("<-", end=" ")
-        elif 0.0 <= y <= 0.5:
-            mouse.Controller().move(0, 10000)
+        elif -1.0 <= y <= 1:
+            mouse.Controller().move(0, screen_height - 5)
             print("^", end=" ")
-        elif screen_height > y >= screen_height - 1:
-            mouse.Controller().move(0, -10000)
+        elif screen_height + 1 > y >= screen_height - 1:
+            mouse.Controller().move(0, -screen_height - 5)
             print("V", end=" ")
 
 
@@ -55,6 +59,51 @@ def on_press(key):
 
 
 def on_release(key):
+    global allowed, current
+    if key == Key.cmd_r:
+        allowed = not allowed
+        if allowed:
+            print("\nSoftware control gained")
+        else:
+            print("\nSoftware control lost")
+
+    if any([key in comb for comb in COMBINATIONS]):  # LINE 2
+        current.add(key)
+        if any(all(k in current for k in comb) for comb in COMBINATIONS):
+            global tnow, tcounter
+            tcounter += 1
+            if datetime.datetime.now() - tnow < datetime.timedelta(seconds=1):
+                if tcounter > 1:
+                    tcounter = 0
+                    listener.stop()
+                    print("\nUn-initialized")
+            else:
+                tnow = datetime.datetime.now()
+                current = set()
+
+
+def mac_on_move(x, y):  # mac and mainly for myself actually
+    # print("{} {}".format(x, y))
+    if allowed:  # well that happened
+        if -0.5 <= x <= 0.5:
+            mouse.Controller().move(screen_width - 5, 0)
+            print("->", end=" ")
+        elif screen_width - 0.5 <= x < screen_width + 0.5:
+            mouse.Controller().move(-screen_width - 5, 0)
+            print("<-", end=" ")
+        elif -0.5 <= y <= 0.5:
+            mouse.Controller().move(0, screen_height - 5)
+            print("^", end=" ")
+        elif screen_height + 0.5 > y >= screen_height - 0.5:
+            mouse.Controller().move(0, -screen_height - 5)
+            print("V", end=" ")
+
+
+def mac_on_press(key):
+    pass
+
+
+def mac_on_release(key):
     global allowed, current
     if key == Key.ctrl_r or key == Key.cmd_r:
         allowed = not allowed
@@ -78,13 +127,114 @@ def on_release(key):
                 current = set()
 
 
+def win_on_move():  # windows version
+    # print("{} {}".format(x, y))
+    if allowed:  # well that happened
+        if -1.0 <= x <= 1:
+            mouse.Controller().move(screen_width - 5, 0)
+            print("->", end=" ")
+        elif screen_width - 1 <= x < screen_width + 1:
+            mouse.Controller().move(-screen_width - 5, 0)
+            print("<-", end=" ")
+        elif -1.0 <= y <= 1:
+            mouse.Controller().move(0, screen_height - 5)
+            print("^", end=" ")
+        elif screen_height + 1 > y >= screen_height - 1:
+            mouse.Controller().move(0, -screen_height - 5)
+            print("V", end=" ")
+
+
+def win_on_press():
+    pass
+
+
+def win_on_release():
+    global allowed, current
+    if key == Key.cmd_r:
+        allowed = not allowed
+        if allowed:
+            print("\nSoftware control gained")
+        else:
+            print("\nSoftware control lost")
+
+    if any([key in comb for comb in COMBINATIONS]):  # LINE 2
+        current.add(key)
+        if any(all(k in current for k in comb) for comb in COMBINATIONS):
+            global tnow, tcounter
+            tcounter += 1
+            if datetime.datetime.now() - tnow < datetime.timedelta(seconds=1):
+                if tcounter > 1:
+                    tcounter = 0
+                    listener.stop()
+                    print("\nUn-initialized")
+            else:
+                tnow = datetime.datetime.now()
+                current = set()
+
+
+def linux_on_move():  # linux version
+    pass
+
+
+def linux_on_press():
+    pass
+
+
+def linux_on_release():
+    pass
+
+
+def mac_ver():  # mainly designed for myself
+    # mac_on_move()
+    # mac_on_press()
+    # mac_on_release()
+    listeners = keyboard.Listener(on_release=mac_on_release, on_press=mac_on_press)
+    listeners.start()
+    with mouse.Listener(on_move=mac_on_move) as listener:
+        listener.join()
+
+
+def windows_ver():  # windows
+    # windows_on_move()
+    # windows_on_press()
+    # windows_on_release()
+    listeners = keyboard.Listener(on_release=windows_on_release, on_press=windows_on_press)
+    listeners.start()
+    with mouse.Listener(on_move=windows_on_move) as listener:
+        listener.join()
+
+
+def linux_ver():  # linux
+    # linux_on_move()
+    # linux_on_press()
+    # linux_on_release()
+    listeners = keyboard.Listener(on_release=linux_on_release, on_press=linux_on_press)
+    listeners.start()
+    with mouse.Listener(on_move=linux_on_move) as listener:
+        listener.join()
+
+
 if __name__ == '__main__':
     s = 1
     allowed = True
-    screen_width = tk.Tk().winfo_screenwidth()
-    screen_height = tk.Tk().winfo_screenheight()
-    listeners = keyboard.Listener(on_release=on_release, on_press=on_press)
-    listeners.start()
-    print("Initialized\nUse right control/right command to pause/resume\nQuit combination:\nCtrl+Esc then Alt+Esc")
-    with mouse.Listener(on_move=on_move) as listener:
-        listener.join()
+    text = "Initialized\nUse right control/right command to pause/resume\nQuit combination:\nCtrl+Esc then Alt+Esc"
+    if system() == "Darwin":
+        print("Mac, Stable")
+        print(text)
+        mac_ver()
+    elif system() == "Windows":
+        print("Windows\nThis is unstable and may slow down your mouse harshly")
+        print(text)
+        windows_ver()
+    elif system() == "Linux":
+        print("Linux\nUntested, unstable, and unfinished. It will not work yet")
+        print(text)
+        linux_ver()
+    else:
+        print("System unknown, this might not work properly")
+        print(text)
+
+    # listeners = keyboard.Listener(on_release=on_release, on_press=on_press)
+    # listeners.start()
+    # with mouse.Listener(on_move=on_move) as listener:
+    #     listener.join()
