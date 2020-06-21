55c17ea77e30fa6ab6274f4d093680e817588f76
Jackson Chen <50755746+JacksonChen666@users.noreply.github.com>
2020-06-21 15:23:47 +0200

    minor changes to bogosort i dont have a life

--- bogosort.py
+++ bogosort.py
@@ -7,13 +7,13 @@ def returnShuffle(lists):
     shuffle(lists)
     return lists
 
-num = int(argv[1]) if len(argv) > 1 else 5
+num = int(argv[1]) if len(argv) >= 2 else 5
 oList = [i for i in range(num)]
 nList = returnShuffle(oList.copy())
 iteration = 0
 bestIter = 100 ** 100
 timeTotal = 0
-printPerIteration = int(argv[2]) if len(argv) > 2 else 1000
+printPerIteration = int(argv[2]) if len(argv) >= 3 and int(argv[2]) > 0 else 1000
 
 print("List size: %s" % len(oList))
 sleep(0.1)
@@ -28,6 +28,6 @@ while bestIter > 0:
         if iteration % printPerIteration == 0: print(f"Iterations: {iteration}", end=" \r", flush=True)
     if iteration < bestIter:
         bestIter = iteration
-        print(f"New best iterations: {bestIter} in {round(timeTotal, 2)}ms ", flush=True)
+        print(f"New best iterations: {bestIter} in {round(timeTotal, 2)}ms", end=" \n", flush=True)
     iteration, timeTotal = 0, 0
     shuffle(nList)
