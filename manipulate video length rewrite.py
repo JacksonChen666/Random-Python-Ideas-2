823245ccdf356d51a9bc22120410c20da5eba93e
Jackson <50755746+JacksonChen666@users.noreply.github.com>
2020-08-11 02:16:57 +0200

    just renamed some functions, and more stuff and a single function can read the time

--- manipulate video length rewrite.py
+++ manipulate video length rewrite.py
@@ -9,48 +9,58 @@ class Video:
         self.format = format or fileName.rpartition(".")[2].lower()
         self.duration, self.dur_loc = 0, 0
         self.time_scale, self.ts_loc = 0, 0
-        self.read_time_scale(limit=limit, force=True)
-        self.read_duration(limit=limit, force=True)
-        self.seconds = self.duration / self.time_scale
+        self.seconds = 0
+        self.read_time(limit=limit, force=True)
 
-    def read_duration(self, limit=1024, force=False):
-        """Rads the duration of the video directly. Time scale is required to calculate the seconds"""
+    @staticmethod
+    def locate(text, start_loc, format):
+        if format == "mp4":
+            return text[text.index(loc_keywords["mp4"][0]) + start_loc:][:4]
 
-        def locate(format=self.format):
-            if format == "mp4":
-                return t[t.index(loc_keywords["mp4"][0]) + 20:][:4]
+    def check(self, text, start_loc, format):
+        if format == "mp4":
+            return loc_keywords["mp4"][0] in text and len(self.locate(text, start_loc, format))
 
-        def check(format=self.format):
-            if format == "mp4":
-                return not (loc_keywords["mp4"][0] in t and len(locate(format)))
+    def _read_duration(self, limit=1024, force=False):
+        """Reads the duration of the video directly. Time scale is required to calculate the seconds"""
+        if not (force or self.duration):
+            return self.duration
 
         with open(self.fileName, "rb") as f:
             t = b""
-            while (len(t) < limit or not limit) and check():
+            while (len(t) < limit or not limit) and not self.check(t, 20, self.format):
                 t += f.read(64)
+            if not self.check(t, 20, self.format):
+                raise Exception("Not found")
 
-        self.duration = int(self.from_hex(locate()), 16)
+        self.duration = int(self.from_hex(self.locate(t, 20, self.format)), 16)
         self.dur_loc = t.index(loc_keywords["mp4"][0]) + 20
         return self.duration
 
-    def read_time_scale(self, limit=1024, force=False):
+    def _read_time_scale(self, limit=1024, force=False):
         """Reads the time scale. Required to translate duration into seconds."""
-
-        def locate(format=self.format):
-            if format == "mp4":
-                return t[t.index(loc_keywords["mp4"][0]) + 16:][:4]
-
-        def check(format=self.format):
-            if format == "mp4":
-                return not (loc_keywords["mp4"][0] in t and len(locate()) == 4)
+        if not (force or self.time_scale):
+            return self.time_scale
 
         with open(self.fileName, "rb") as f:
             t = b""
-            while (len(t) < limit or not limit) and check():
+            while (len(t) < limit or not limit) and not self.check(t, 16, self.format):
                 t += f.read(64)
+            if not self.check(t, 16, self.format):
+                raise Exception("Not found")
 
-        self.time_scale = int(self.from_hex(locate()), 16)
+        self.time_scale = int(self.from_hex(self.locate(t, 16, self.format)), 16)
         self.ts_loc = t.index(loc_keywords["mp4"][0]) + 16
+        return self.time_scale
+
+    def read_time(self, limit=1024, force=False):
+        if not (force or self.seconds):
+            return self.seconds
+
+        self._read_duration(limit=limit, force=force)
+        self._read_time_scale(limit=limit, force=force)
+        self.seconds = self.duration / self.time_scale
+        return self.seconds
 
     @staticmethod
     def to_hex_bytes(_int):
@@ -73,5 +83,5 @@ class Video:
 
 
 if __name__ == '__main__':
-    e = Video("mp4.mp4", limit=541232451425)
+    e = Video("mp4.mp4")
     print(e)
